199dd520b247add402ce39ddf235c8d27b216caf
LaBaFan <1391193705@qq.com>
2025-01-08 13:51:42 +0800

    笔记格式更新

--- mkdocs-site/site/javascripts/mathjax.js
+++ mkdocs-site/site/javascripts/mathjax.js
@@ -1,11 +1,17 @@
    window.MathJax = {
      tex: {
        inlineMath: [['$', '$'], ['\\(', '\\)']],
-       displayMath: [['$$', '$$'], ['\\[', '\\]']]
+       displayMath: [['$$', '$$'], ['\\[', '\\]']],
+       processEscapes: true,
+       processEnvironments: true,
      },
      options: {
        skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
-       ignoreHtmlClass: 'tex2jax_ignore',
-       processHtmlClass: 'tex2jax_process'
-     }
+       ignoreHtmlClass: '.*|',
+       processHtmlClass: 'arithmatex',
+     },
    };
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
